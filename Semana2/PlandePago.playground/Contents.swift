b8b225518e29f27465ac441a7d1f91e1f1087de2
kevinquispe-hash <kevquispecc@gmail.com>
2026-08-26 10:19:02 -0500

    paso2 : pagos

--- Semana2/PlandePago.playground/Contents.swift
+++ Semana2/PlandePago.playground/Contents.swift
@@ -11,3 +11,22 @@ let cantidad = Int(readLine() ?? "0") ?? 0
 // calculo total
 let montoTotalCompra = precioUnit * Double(cantidad)
 print("\nMONTO TOTAL: S/ \(montoTotalCompra)")
+
+//pagoo
+print("Elige el plan de pago (6, 12, 24):")
+let planCuotas = Int(readLine() ?? "0") ?? 6
+var porcentajeInteres: Double = 0.0
+
+switch planCuotas {
+case 6:
+    porcentajeInteres = 0.20
+case 12:
+    porcentajeInteres = 0.40
+case 24:
+    porcentajeInteres = 0.60
+default:
+    porcentajeInteres = 0.0
+}
+let interesMonto = montoTotalCompra * porcentajeInteres
+let montoFinal = montoTotalCompra + interesMonto
+let montoCuota = montoFinal / Double(planCuotas)
