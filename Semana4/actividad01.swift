0b03818551cfd3d02a7d1bd09d352ce368dce25c
Kevin Quispe <kevquispecc@gmail.com>
2026-09-14 11:37:47 -0500

    Actividad01 - calculo de subtotal e igv

--- Semana4/actividad01.swift
+++ Semana4/actividad01.swift
@@ -13,3 +13,9 @@ let cursos: [Curso] = [
     Curso(nombre: "IA con Python", precioUnitario: 550.00, cantidad: 2),
     Curso(nombre: "Diseño UI", precioUnitario: 400.00, cantidad: 1)
 ]
+var subtotal = 0.0
+for curso in cursos {
+    subtotal += curso.precioUnitario * Double(curso.cantidad)
+}
+let igv = subtotal * 0.18
+let totalConIGV = subtotal + igv
