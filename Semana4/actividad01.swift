9b69b8e7d3108c2f7cd1516594187c9d9b718ab6
Kevin Quispe <kevquispecc@gmail.com>
2026-09-14 11:35:02 -0500

    Actividad01 - datos del estudiante y cursos

---
+++ Semana4/actividad01.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct Curso {
+    let nombre: String
+    let precioUnitario: Double
+    let cantidad: Int
+}
+let nombreEstudiante = "Juan Leon"
+let dni = "78965412"
+let esAlumnoTecsup = true
+let cursos: [Curso] = [
+    Curso(nombre: "Swift Avanzado", precioUnitario: 450.00, cantidad: 1),
+    Curso(nombre: "IA con Python", precioUnitario: 550.00, cantidad: 2),
+    Curso(nombre: "Diseño UI", precioUnitario: 400.00, cantidad: 1)
+]
